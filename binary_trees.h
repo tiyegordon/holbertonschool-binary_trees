ee21eec5d795a5268506a5561c14d9e7ffe93b56
tiyegordon <tiye.gordon@gmail.com>
2025-08-08 20:42:38 -0400

    task 1

--- binary_trees.h
+++ binary_trees.h
@@ -52,4 +52,12 @@ int binary_tree_is_perfect(const binary_tree_t *tree);
 binary_tree_t *binary_tree_sibling(binary_tree_t *node);
 binary_tree_t *binary_tree_uncle(binary_tree_t *node);
 
+binary_tree_t *SetNewNode(int value);
+int right_height(const binary_tree_t *tree);
+int left_height(const binary_tree_t *tree);
+int is_full_recursive(const binary_tree_t *tree);
+size_t binary_tree_children(const binary_tree_t *tree);
+size_t binary_tree_leaves(const binary_tree_t *tree);
+void binary_tree_print(const binary_tree_t *);
+
 #endif /* _BINARY_TREES_H_ */
